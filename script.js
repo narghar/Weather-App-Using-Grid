151ce30a16c3825ef8e8aa3cb4a4c44cb0f72040
mdwiltfong <wiltfong.michael1@gmail.com>
2021-04-11 22:52:38 -0400

    changes to main.js
    Api is communicating and rendering images

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const openWeatherKey = '9bd9831354bba0cbdcdeb62969eb416f';
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 const userInput = document.getElementsByTagName('input');
 const btn = document.getElementById('button');
-
+const days = [`Sunday`, `Monday`, `Tuesday`, `Wednesday`,`Thursday`,`Friday`,`Saturday`];
+const today = new Date();
 // AJAX Function 
 const getForecast = async () => {
     let city = userInput[0].value;
@@ -11,27 +12,40 @@ const getForecast = async () => {
     try {
         const response = await fetch(urlToFetch);
         if (response.ok) {
-            // console.log(response.json());
-            var jsonResponse = await response.json();
-            const city_name = JSON.stringify(jsonResponse.city.name);
-            console.log(city_name);
-            const forecasts_40 = jsonResponse.list;
-            console.log(forecasts_40);
-
-            return city_name;
-
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            return jsonResponse;
         }
     } catch (error) {
         console.log('error');
     }
 }
 
-btn.onclick = getForecast;
-console.log(getForecast());
 
+
+function executeSearch() {
+getForecast().then( forecast => createForecastHTML(forecast));
+
+
+}
+btn.onclick = executeSearch;
 //Rendering Functions
 
 function createForecastHTML(day) {
+    
     let city = document.getElementById('city');
-    city.append(`<p> ${getForecast.city.name}`);
+    city.innerText = `${day.city.name}`
+    let Day = document.getElementById('day');
+    Day.innerText = `${days[today.getDay()]}`;
+    let time = document.getElementById('time');
+    time.innerText = `${today.getHours()}:${today.getMinutes()}`
+    let weather = document.getElementById('Weather');
+    weather.innerText = `${day.list[0].weather[0].description}`
+    let icon = document.getElementById('icon');
+    icon.innerHTML= `<img src="https://openweathermap.org/img/wn/${day.list[0].weather[0].icon}@2x.png">`
+    let temp = document.getElementById('temp');
+}
+
+function convertK2C(k){
+    return k-273.15;
 }
