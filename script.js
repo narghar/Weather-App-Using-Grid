f9e722840deff6826c01e058b10e6da288be564c
mdwiltfong <wiltfong.michael1@gmail.com>
2021-04-21 22:04:56 -0400

    changes to script.js
    added geolocation api. Currently trying to make it work with openweatherAPI.

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 // OpenWeather Info
 const openWeatherKey = '9bd9831354bba0cbdcdeb62969eb416f';
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/';
+
 const userInput = document.getElementsByTagName('input');
 const btn = document.getElementById('button');
-const days = [`Sunday`, `Monday`, `Tuesday`, `Wednesday`,`Thursday`,`Friday`,`Saturday`];
+const days = [`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`];
 const today = new Date();
+//Elements
+const city = document.getElementById('city');
+const Day = document.getElementById('day');
+const time = document.getElementById('time');
+const weather = document.getElementById('Weather');
+const icon = document.getElementById('icon');
+const temp = document.getElementById('temp');
+
+
+// Geolocation API
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, fail);
+} else {
+    alert(`Hmm, you'll need to allow geolocation for the app to work. Please reload the page`)
+}
+
+function success(position){
+    let lon = position.coords.longitude;
+    let lat = position.coords.latiude;
+    let fetchByLocation=`${weatherUrl}weather?lat=${lat}&lon=${lon}&appid=${openWeatherKey}`;
+    return fetchByLocation;
+}
+
+function fail() {
+    alert(`Hmm, you'll need to allow geolocation for the app to work. Please reload the page`)
+}
+
+
 // AJAX Function 
-const getForecast = async () => {
+
+
+const getForecast = async (url) => {
     let city = userInput[0].value;
-    const urlToFetch = `${weatherUrl}?&q=${city}&APPID=${openWeatherKey}`;
+    const urlToFetch = `${weatherUrl}forecast?&q=${city}&APPID=${openWeatherKey}`;
     try {
         const response = await fetch(urlToFetch);
         if (response.ok) {
@@ -24,28 +55,19 @@ const getForecast = async () => {
 
 
 function executeSearch() {
-getForecast().then( forecast => createForecastHTML(forecast));
-
-
+    getForecast().then(forecast => createForecastHTML(forecast));
 }
 btn.onclick = executeSearch;
 //Rendering Functions
 
 function createForecastHTML(day) {
-    
-    let city = document.getElementById('city');
     city.innerText = `${day.city.name}`
-    let Day = document.getElementById('day');
     Day.innerText = `${days[today.getDay()]}`;
-    let time = document.getElementById('time');
     time.innerText = `${today.getHours()}:${today.getMinutes()}`
-    let weather = document.getElementById('Weather');
     weather.innerText = `${day.list[0].weather[0].description}`
-    let icon = document.getElementById('icon');
-    icon.innerHTML= `<img src="https://openweathermap.org/img/wn/${day.list[0].weather[0].icon}@2x.png">`
-    let temp = document.getElementById('temp');
+    icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${day.list[0].weather[0].icon}@2x.png">`
 }
 
-function convertK2C(k){
-    return k-273.15;
+function convertK2C(k) {
+    return k - 273.15;
 }
