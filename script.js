cb8204d1ec76ecfae3a6fa515132d7aa8989b579
mdwiltfong <wiltfong.michael1@gmail.com>
2021-04-23 14:36:47 -0400

    Update script.js
    fixed geolocation api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // OpenWeather Info
-const openWeatherKey = '014e191a87bb557f510dab69fcf6462c';
+const openWeatherKey = '9b93f17aff8285d1973f1ba4a3968621';
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/';
 const userInput = document.getElementsByTagName('input');
 const btn = document.getElementById('button');
@@ -20,26 +20,19 @@ if (navigator.geolocation) {
     alert(`Hmm, you'll need to allow geolocation for the app to work. Please reload the page`)
 }
 
-function success(position){
- let lat=position.coords.latitude;
- let lon=position.coords.longitude;
- let url=`${weatherUrl}weather?lat=${lat}&lon=${lon}$appid=${openWeatherKey}`;
- console.log(url);
- try {
-    const response =  fetch(url);
-    if (response.ok) {
-        const jsonResponse =  response.json();
-        console.log(jsonResponse);
-        return jsonResponse;
-    }
-} catch (error) {
-    console.log('error');
-}
+function success(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherKey}`;
+    console.log(url);
+    const response = fetch(url);
+    const jsonreponse=response.json();
+    return jsonreponse.then(forecast => createForecastHTML(forecast));
 }
 
 
 function fail() {
-    alert(`Hmm, you'll need to allow geolocation for the app to work. Please reload the page`)
+    alert(`Hmm, you'll need to allow geolocation for the app to work. Once you do, please reload the page`)
 }
 
 
@@ -61,16 +54,15 @@ const getForecast = async (url) => {
     }
 }
 
-
-
 function executeSearch() {
     getForecast().then(forecast => createForecastHTML(forecast));
 }
 btn.onclick = executeSearch;
 //Rendering Functions
-
+console.log(city)
 function createForecastHTML(day) {
-    city.innerText = `${day.city.name}`
+    city.innerText = `${day.name}`
+    console.log(city);
     Day.innerText = `${days[today.getDay()]}`;
     time.innerText = `${today.getHours()}:${today.getMinutes()}`
     weather.innerText = `${day.list[0].weather[0].description}`
